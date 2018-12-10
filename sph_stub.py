c345e9e04ca866ca5a18dafb89b3af4909d52faa
oliver boom <ob14153@my.bristol.ac.uk>
2018-12-10 17:41:26 +0000

    Local commit

--- sph_stub.py
+++ sph_stub.py
@@ -1,7 +1,7 @@
 """SPH class to find nearest neighbours..."""
 
 from itertools import count
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -102,6 +102,47 @@ class SPH_particle(object):
                                  (2.0*self.main_data.h), int)
 
 
+def dw_dr(r, h):
+   constant = 10/(7*np.pi*h**2)
+   rm = np.sqrt(r[0]**2+r[1]**2)
+   q = rm/h
+   print(q)
+   if q<=1 and q>=0:
+       value = constant*(-3*rm+9*rm**2/4)
+   elif q>=1 and q<=2:
+       value = constant* (-3*(2-rm/h)**2/(4*h))
+   else:
+       value = 0
+   return value
+
+
+def w(r, h):
+   constant = 10/(7*np.pi*h**2)
+   rm = np.sqrt(r[0]**2+r[1]**2)
+   q = rm/h
+   if q<=1 and q>=0:
+       value = constant*(1-1.5*q**2+0.75*q**3)
+   elif q>=1 and q<=2:
+       value = constant* 0.25*(2-q)**3
+   else:
+       value = 0
+   return value
+
+
+def dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu):
+    mag_r = np.sqrt(sum(rij ** 2))
+    e_ij = rij / mag_r
+    dv = - m2 * (P1/rho_1**2 + P2/rho_2**2) * dw_dr(rij, h) * e_ij + (mu * m2 + (rho_1 ** -2 + rho_2 ** -2) * dw_dr(rij, h) * vij / mag_r)
+    return dv
+
+rij = np.array([1, 1])
+vij = np.array([2, 3])
+mag_r = np.sqrt(sum(rij ** 2))
+
+e_ij = rij / mag_r
+h = 1.3 * 5
+print(dv_dt(1, 2, 1.225, 1.225, rij, vij, h, 0.01, 0.001))
+
 """Create a single object of the main SPH type"""
 domain = SPH_main()
 
@@ -117,4 +158,16 @@ domain.place_points(domain.min_x, domain.max_x)
 """This function needs to be called at each time step (or twice a time step if a second order time-stepping scheme is used)"""
 domain.allocate_to_grid()
 """This example is only finding the neighbours for a single partle - this will need to be inside the simulation loop and will need to be called for every particle"""
-domain.neighbour_iterate(domain.particle_list[100])
+# domain.neighbour_iterate(domain.particle_list[100])
+# for i in range(domain.particle_list):
+#     for j in range(1, domain.particle_list):
+#         dv = dv_dt()
+
+
+fig, ax1 = plt.subplots(1, 1, figsize=(10, 5))
+
+
+# for i in range(0, len(domain.particle_list)):
+#     ax1.plot(domain.particle_list[i].x[0], domain.particle_list[i].x[1], 'bo')
+#
+# plt.show()
