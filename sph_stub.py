45a895be619de36a2d45b0be78eb8f5a331646d4
oliver boom <ob14153@my.bristol.ac.uk>
2018-12-10 18:17:51 +0000

    Adding some differential equations

--- sph_stub.py
+++ sph_stub.py
@@ -27,19 +27,19 @@ class SPH_main(object):
         self.max_x[:] = (1.0, 1.0)
         self.dx = 0.02
         self.h_fac = 2
-        self.h = self.dx*self.h_fac
+        self.h = self.dx * self.h_fac
 
     def initialise_grid(self):
         """Initalise simulation grid."""
 
         """Increases the minimum and maximum to account for the virtual particle padding that is required at boundaries"""
-        self.min_x -= 2.0*self.h
-        self.max_x += 2.0*self.h
-        
+        self.min_x -= 2.0 * self.h
+        self.max_x += 2.0 * self.h
+
         """Calculates the size of the array required to store the search array"""
-        self.max_list = np.array((self.max_x-self.min_x)/(2.0*self.h)+1,
+        self.max_list = np.array((self.max_x - self.min_x) / (2.0 * self.h) + 1,
                                  int)
-                                 
+
         self.search_grid = np.empty(self.max_list, object)
 
     def place_points(self, xmin, xmax):
@@ -77,15 +77,15 @@ class SPH_main(object):
 
     def neighbour_iterate(self, part):
         """Find all the particles within 2h of the specified particle"""
-        for i in range(max(0, part.list_num[0]-1),
-                       min(part.list_num[0]+2, self.max_list[0])):
-            for j in range(max(0, part.list_num[1]-1),
-                           min(part.list_num[1]+2, self.max_list[1])):
+        for i in range(max(0, part.list_num[0] - 1),
+                       min(part.list_num[0] + 2, self.max_list[0])):
+            for j in range(max(0, part.list_num[1] - 1),
+                           min(part.list_num[1] + 2, self.max_list[1])):
                 for other_part in self.search_grid[i, j]:
                     if part is not other_part:
-                        dn = part.x-other_part.x
-                        dist = np.sqrt(np.sum(dn**2))
-                        if dist < 2.0*self.h:
+                        dn = part.x - other_part.x
+                        dist = np.sqrt(np.sum(dn ** 2))
+                        if dist < 2.0 * self.h:
                             """This is only for demonstration - Your code will need to do all the particle to particle calculations at this point rather than simply displaying the vector to the neighbour"""
                             print("id:", other_part.id, "dn:", dn)
 
@@ -107,66 +107,114 @@ class SPH_particle(object):
 
     def calc_index(self):
         """Calculates the 2D integer index for the particle's location in the search grid"""
-        self.list_num = np.array((self.x-self.main_data.min_x) /
-                                 (2.0*self.main_data.h), int)
+        self.list_num = np.array((self.x - self.main_data.min_x) /
+                                 (2.0 * self.main_data.h), int)
 
 
 def dw_dr(r, h):
-   constant = 10/(7*np.pi*h**2)
-   rm = np.sqrt(r[0]**2+r[1]**2)
-   q = rm/h
-   print(q)
-   if q<=1 and q>=0:
-       value = constant*(-3*rm+9*rm**2/4)
-   elif q>=1 and q<=2:
-       value = constant* (-3*(2-rm/h)**2/(4*h))
-   else:
-       value = 0
-   return value
+    """
+    Function to give the gradient of the smoothing kernel,
+    includes compact support
+    :param r: the positional vector between particle i and j
+    :param h: the characteristic smoothing lenght
+    :return:
+    The gradient of the scaling factor associated with the smoothing kernel
+    """
+    constant = 10 / (7 * np.pi * h ** 2)
+    r_mag = np.sqrt(r[0] ** 2 + r[1] ** 2)
+    q = r_mag / h
+    if 1 >= q >= 0:
+        value = constant * (-3 * r_mag + 9 * r_mag ** 2 / 4)
+    elif q >= 1 and q <= 2:
+        value = constant * (-3 * (2 - r_mag / h) ** 2 / (4 * h))
+    else:
+        value = 0
+    return value
 
 
 def w(r, h):
-   constant = 10/(7*np.pi*h**2)
-   rm = np.sqrt(r[0]**2+r[1]**2)
-   q = rm/h
-   if q<=1 and q>=0:
-       value = constant*(1-1.5*q**2+0.75*q**3)
-   elif q>=1 and q<=2:
-       value = constant* 0.25*(2-q)**3
-   else:
-       value = 0
-   return value
+    """
+    Using a smoothing kernel returns the quantity A at location
+    x
+    :param r: the positional vector between particle i and j
+    :param h: the characteristic smoothing length
+    :return:
+    The scaling factor associated with the smoothing kernel
+    """
+    constant = 10 / (7 * np.pi * h ** 2)
+    rm = np.sqrt(r[0] ** 2 + r[1] ** 2)
+    q = rm / h
+    if q <= 1 and q >= 0:
+        value = constant * (1 - 1.5 * q ** 2 + 0.75 * q ** 3)
+    elif q >= 1 and q <= 2:
+        value = constant * 0.25 * (2 - q) ** 3
+    else:
+        value = 0
+    return value
 
 
 def dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu):
+    """
+    Calculates the change in velocity for a time step
+    :return:
+    """
     mag_r = np.sqrt(sum(rij ** 2))
-    e_ij = rij / mag_r
-    dv = - m2 * (P1/rho_1**2 + P2/rho_2**2) * dw_dr(rij, h) * e_ij + (mu * m2 + (rho_1 ** -2 + rho_2 ** -2) * dw_dr(rij, h) * vij / mag_r)
+    eij = rij / mag_r
+    dv = - m2 * (P1 / rho_1 ** 2 + P2 / rho_2 ** 2) * dw_dr(rij, h) * eij + (
+                mu * m2 + (rho_1 ** -2 + rho_2 ** -2) * dw_dr(rij, h) * vij / mag_r)
     return dv
+
+def drho_dt(rij, vij, h, m2):
+    """
+    Calculates the change in density for a time step
+    :return:
+    """
+    mag_r = np.sqrt(sum(rij ** 2))
+    eij = rij / mag_r
+    drho = m2 * dw_dr(rij, h) * np.dot(vij, eij)
+    return drho
+
+
+
+def ode_equation():
+    """
+    The list of equations which describes the system
+    :return:
+    """
+    f = np.zeros_like()
+    f[0] = #dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu)
+    f[1] = #dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu)
+    f[2] = #drho_dt(rij, vij, h, m2)
+    f[3]
+    return f
+
 
 rij = np.array([1, 1])
 vij = np.array([2, 3])
 mag_r = np.sqrt(sum(rij ** 2))
-
+m2 = 5
 e_ij = rij / mag_r
 h = 1.3 * 5
 print(dv_dt(1, 2, 1.225, 1.225, rij, vij, h, 0.01, 0.001))
+print(drho_dt(rij, vij, h, m2))
 
-"""Create a single object of the main SPH type"""
-domain = SPH_main()
 
-"""Calls the function that sets the simulation parameters"""
-domain.set_values()
-"""Initialises the search grid""" 
-domain.initialise_grid()
 
-"""Places particles in a grid over the entire domain - In your code you will need to place the fluid particles in only the appropriate locations"""
-domain.place_points(domain.min_x, domain.max_x)
-
-"""This is only for demonstration only - In your code these functions will need to be inside the simulation loop"""
-"""This function needs to be called at each time step (or twice a time step if a second order time-stepping scheme is used)"""
-domain.allocate_to_grid()
-"""This example is only finding the neighbours for a single partle - this will need to be inside the simulation loop and will need to be called for every particle"""
+# """Create a single object of the main SPH type"""
+# domain = SPH_main()
+#
+# """Calls the function that sets the simulation parameters"""
+# domain.set_values()
+# """Initialises the search grid"""
+# domain.initialise_grid()
+#
+# """Places particles in a grid over the entire domain - In your code you will need to place the fluid particles in only the appropriate locations"""
+# domain.place_points(domain.min_x, domain.max_x)
+#
+# """This is only for demonstration only - In your code these functions will need to be inside the simulation loop"""
+# """This function needs to be called at each time step (or twice a time step if a second order time-stepping scheme is used)"""
+# domain.allocate_to_grid()
+# """This example is only finding the neighbours for a single partle - this will need to be inside the simulation loop and will need to be called for every particle"""
 
 # domain.neighbour_iterate(domain.particle_list[100])
 # for i in range(domain.particle_list):
@@ -174,24 +222,22 @@ domain.allocate_to_grid()
 #         dv = dv_dt()
 
 
-fig, ax1 = plt.subplots(1, 1, figsize=(10, 5))
-
+# fig, ax1 = plt.subplots(1, 1, figsize=(10, 5))
 
 # for i in range(0, len(domain.particle_list)):
 #     ax1.plot(domain.particle_list[i].x[0], domain.particle_list[i].x[1], 'bo')
 #
 # plt.show()
 
-domain.neighbour_iterate(domain.particle_list[100])
+# domain.neighbour_iterate(domain.particle_list[100])
 
-x_value = []
-y_value = []
+# x_value = []
+# y_value = []
 
-for value in domain.particle_list:
-
-    x_value.append(value.x[0])
-    y_value.append(value.x[1])
-    print(x_value)
-    print(y_value)
-ax3 = plt.subplot(111)
-ax3.plot(x_value, y_value, 'b.')
+# for value in domain.particle_list:
+#     x_value.append(value.x[0])
+#     y_value.append(value.x[1])
+#     print(x_value)
+#     print(y_value)
+# ax3 = plt.subplot(111)
+# ax3.plot(x_value, y_value, 'b.')
