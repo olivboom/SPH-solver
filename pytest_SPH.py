14ad8bfdc55aa6b4bff464541591b9040906fd7c
Keer Mei <keermei1992@gmail.com>
2018-12-10 20:23:23 +0100

    working test file

--- pytest_SPH.py
+++ pytest_SPH.py
@@ -1,23 +1,12 @@
-from sph_stub import SPH_main
+import sph_stub
 
-
-domain = SPH_main()
-domain.set_values()
-domain.initialise_grid()
-domain.place_points(domain.min_x, domain.max_x)
-domain.allocate_to_grid()
-
-x_value = []
-y_value = []
-
-for value in domain.particle_list:
-   x_value.append(value.x[0])
-   y_value.append(value.x[1])
-
-assert len(x_value) != 0
-assert len(y_value) != 0
-
-def check_grid():
+def test_grid():
+    domain = sph_stub.SPH_main()
+    domain.set_values()
+    domain.initialise_grid()
+    domain.place_points(domain.min_x, domain.max_x)
+    domain.allocate_to_grid()
+    
     x_value = []
     y_value = []
 
@@ -28,14 +17,23 @@ def check_grid():
     assert len(x_value) != 0
     assert len(y_value) != 0
 
-def check_speed():
+def test_speed():
+    domain = sph_stub.SPH_main()
+    domain.set_values()
+    domain.initialise_grid()
+    domain.place_points(domain.min_x, domain.max_x)
+    domain.allocate_to_grid()
+    
     for value in domain.particle_list:
         assert value.v[0] == 0
         assert value.v[1] == 0
 
-def check_density():
+def test_density():
+    domain = sph_stub.SPH_main()
+    domain.set_values()
+    domain.initialise_grid()
+    domain.place_points(domain.min_x, domain.max_x)
+    domain.allocate_to_grid()
+    
     for value in domain.particle_list:
         assert value.rho == 0
-
-def test_answer():
-    assert 3 == 3
