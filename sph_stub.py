09bdeefc4ad68c762cd17d4aca96f1d7de73b9b5
oliver boom <ob14153@my.bristol.ac.uk>
2018-12-11 10:32:41 +0000

    Moved the calculations from function to method

--- sph_stub.py
+++ sph_stub.py
@@ -11,6 +11,7 @@ class SPH_main(object):
         self.h = 0.0
         self.h_fac = 0.0
         self.dx = 0.0
+        self.mu = 0.001
 
         self.min_x = np.zeros(2)
         self.max_x = np.zeros(2)
@@ -74,6 +75,7 @@ class SPH_main(object):
         for cnt in self.particle_list:
             self.search_grid[cnt.list_num[0], cnt.list_num[1]].append(cnt)
 
+
     def neighbour_iterate(self, part):
         """Find all the particles within 2h of the specified particle"""
         for i in range(max(0, part.list_num[0] - 1),
@@ -85,7 +87,14 @@ class SPH_main(object):
                         dn = part.x - other_part.x
                         dist = np.sqrt(np.sum(dn ** 2))
                         if dist < 2.0 * self.h:
-                            """This is only for demonstration - Your code will need to do all the particle to particle calculations at this point rather than simply displaying the vector to the neighbour"""
+
+                            part.a += (- other_part.m * (part.P / part.rho ** 2 + other_part.P/ other_part.rho ** 2) *
+                            dw_dr(dist / self.h, self.h) * dn / dist + (
+                                             self.mu * other_part.m * (part.rho ** -2 + other_part.rho ** -2) *
+                                             dw_dr(dist / self.h, self.h) * part.v / dist))
+
+                            print('Acceleration', part.a)
+
                             print("id:", other_part.id, "dn:", dn)
 
 
@@ -110,7 +119,7 @@ class SPH_particle(object):
                                  (2.0 * self.main_data.h), int)
 
 
-def dw_dr(r, h):
+def dw_dr(q, h):
     """
     Function to give the gradient of the smoothing kernel,
     includes compact support
@@ -119,19 +128,16 @@ def dw_dr(r, h):
     :return:
     The gradient of the scaling factor associated with the smoothing kernel
     """
-    constant = 10 / (7 * np.pi * h ** 2)
-    r_mag = np.sqrt(r[0] ** 2 + r[1] ** 2)
-    q = r_mag / h
     if 1 >= q >= 0:
-        value = constant * (-3 * r_mag + 9 * r_mag ** 2 / 4)
+        value = 10 / (7 * np.pi * h ** 2) * (-3 * q + 2.25 * q ** 2)
     elif q >= 1 and q <= 2:
-        value = constant * (-3 * (2 - r_mag / h) ** 2 / (4 * h))
+        value = 10 / (7 * np.pi * h ** 2) * -0.75 * (2 - q) ** 2
     else:
         value = 0
     return value
 
 
-def w(r, h):
+def w(q, h):
     """
     Using a smoothing kernel returns the quantity A at location
     x
@@ -140,36 +146,17 @@ def w(r, h):
     :return:
     The scaling factor associated with the smoothing kernel
     """
-    constant = 10 / (7 * np.pi * h ** 2)
-    rm = np.sqrt(r[0] ** 2 + r[1] ** 2)
-    q = rm / h
     if q <= 1 and q >= 0:
-        value = constant * (1 - 1.5 * q ** 2 + 0.75 * q ** 3)
+        value = 10 / (7 * np.pi * h ** 2) * (1 - 1.5 * q ** 2 + 0.75 * q ** 3)
     elif q >= 1 and q <= 2:
-        value = constant * 0.25 * (2 - q) ** 3
+        value = 10 / (7 * np.pi * h ** 2) * 0.25 * (2 - q) ** 3
     else:
         value = 0
     return value
 
 
-def dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu):
-    """
-    Calculates the change in velocity for a time step
-    for a
-    :return:
-    """
-    mag_r = np.sqrt(sum(rij ** 2))
-    eij = rij / mag_r
-    dv = - m2 * (P1 / rho_1 ** 2 + P2 / rho_2 ** 2) * dw_dr(rij, h) * eij + (
-                mu * m2 + (rho_1 ** -2 + rho_2 ** -2) * dw_dr(rij, h) * vij / mag_r)
-    return dv
-
-
 def drho_dt(rij, vij, h, m2):
-    """
-    Calculates the change in density for a time step
-    :return:
-    """
+    """Calculates the change in density for a time step"""
     mag_r = np.sqrt(sum(rij ** 2))
     eij = rij / mag_r
     drho = m2 * dw_dr(rij, h) * np.dot(vij, eij)
@@ -183,21 +170,27 @@ def pressure(rho, rho0, c0, gamma):
     return pres
 
 
-def forward_euler(state, P1, P2, rho_1, rho_2, mu, c0, dt):
-    """
-    Because the equations are not coupled you can just time
-    step through
-    :return:
-    """
-    # Want to check that the right indices are taken in the right places
-    vij = np.array((state[0], state[1]))
-    dv = dv_dt(P1, P2, rho_1, rho_2, rij, vij, h, m2, mu)
-
-    state[0] = state[0] + dt * dv[0]
-    state[1] = state[1] + dt * dv[1]
-    state[2] = state[2] + dt * drho_dt(rij, vij, h, m2)
-    state[3] = pressure(rho, rho0, c0, gamma)
-    return state
+# def forward_euler(rij, state, P2, rho_2, mu, c0, dt):
+#     """
+#     Because the equations are not coupled you can just time
+#     step through
+#     Variables of interest are v1, v2, rho and P1
+#     The 'State' variable contains these quantities
+#     :param state: [v1, v2, rho1, P1]
+#     :param P2:
+#     :param rho_2:
+#     :param mu:
+#     :param c0:
+#     :param dt:
+#     :return:
+#     """
+#     dv = dv_dt(rij, np.array((state[0], state[1])), state[2], state[3], P2, rho_2, h, m2, mu)
+#     print('DV:', dv)
+#     state[0] = state[0] + dt * dv[0]
+#     state[1] = state[1] + dt * dv[1]
+#     state[2] = state[2] + dt * drho_dt(rij, vij, h, m2)
+#     state[3] = pressure(rho, rho0, c0, gamma)
+#     return state
 
 
 # """Create a single object of the main SPH type"""
@@ -217,39 +210,6 @@ domain.allocate_to_grid()
 """This example is only finding the neighbours for a single partle - this will need to be inside the simulation loop and will need to be called for every particle"""
 # domain.neighbour_iterate(domain.particle_list[100])
 
-rij = np.array([1, 1])
-vij = np.array([2, 3])
-mag_r = np.sqrt(sum(rij ** 2))
-m2 = 5
-gamma = 1
-rho = 1.2
-rho0 = 1.225
-e_ij = rij / mag_r
-h = 1.3 * 5
-dt = 1
-mu = 0.001
-c0 = 20
-
-i = 0
-j = 1
-P1 = domain.particle_list[i].P
-P2 = domain.particle_list[j].P
-rho_1 = domain.particle_list[i].rho
-rho_2 = domain.particle_list[j].rho
-
-state = [domain.particle_list[i].v[0], domain.particle_list[i].v[1], domain.particle_list[i].rho, domain.particle_list[i].P]
-print(state)
-state = forward_euler(state, P1, P2, rho_1, rho_2, mu, c0, dt)
-print(state)
-
-
-for i in range(len(domain.particle_list)):
-    print(i)
-    for j in range(1, len(domain.particle_list)):
-        print(j)
-        state = forward_euler(state, P1, P2, rho_1, rho_2, mu, c0, dt)
-
-
 
 # fig, ax1 = plt.subplots(1, 1, figsize=(10, 5))
 
@@ -258,7 +218,8 @@ for i in range(len(domain.particle_list)):
 #
 # plt.show()
 
-# domain.neighbour_iterate(domain.particle_list[100])
+for i in range(len(domain.particle_list)):
+    domain.neighbour_iterate(domain.particle_list[i])
 
 # x_value = []
 # y_value = []
