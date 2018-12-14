753cd86257f0bb49e90ead524516a4cf345a1894
Keer Mei <keermei1992@gmail.com>
2018-12-14 11:18:34 +0100

    final submission

--- front_end.py
+++ front_end.py
@@ -1,13 +1,11 @@
 import sph as sph
-import post_processor as post
-import matplotlib.pyplot as plt
 
 min_x = (0.0, 0.0)
 max_x = (20.0, 10.0)
-dx = 1
+dx = 0.5
 h_fac = 1.3
 b_width = 3
-t_max = 5
+t_max = 1
 geometry = 'default'
 w_force = 'else'
 q_fac = 0.1
@@ -17,7 +15,6 @@ sph.run(min_x=min_x, max_x=max_x, dx=dx, h_fac=h_fac,b_width=b_width, t_max=t_ma
         geometry=geometry, w_force=w_force, q_fac=q_fac, bounce=bounce)
 
 option = 3 # colour bars /// 1: vx, 2: vy, 3: density, 4: pressure
+import postprocessor as post
 
-
-post.run(option)
 
